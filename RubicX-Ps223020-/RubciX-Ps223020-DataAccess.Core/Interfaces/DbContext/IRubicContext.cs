ece1f587c845fc75a920140af81668cffe2101cf
StudentDev <vovaklinov007@gmail.com>
2021-11-20 16:11:57 +0300

    124
    346

---
+++ RubicX-Ps223020-/RubciX-Ps223020-DataAccess.Core/Interfaces/DbContext/IRubicContext.cs
@@ -0,0 +1,18 @@
+﻿using RubciX_Ps223020_DataAccess.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RubciX_Ps223020_DataAccess.Core.Interfaces.DbContext
+{
+    interface IRubicContext : IDisposable, IAsyncDisposable
+    {
+        DbSet<UserRto> Users { get; set; }
+        DbSet<UserRoleRto> UserRoles { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
