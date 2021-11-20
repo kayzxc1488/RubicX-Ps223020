0be6c765990de06614c4a440e525d9822fd40df5
StudentDev <vovaklinov007@gmail.com>
2021-11-20 17:09:47 +0300

    124
    235

--- RubicX-Ps223020-/RubicX-Ps223020-DatAccess/DbContext/RubicContext.cs
+++ RubicX-Ps223020-/RubicX-Ps223020-DatAccess/DbContext/RubicContext.cs
@@ -1,15 +1,34 @@
 ﻿using RubciX_Ps223020_DataAccess.Core.Interfaces.DbContext;
+using RubciX_Ps223020_DataAccess.Core.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RubicX_Ps223020_DatAccess.DbContext
 {
     public class RubicContext : IRubicContext
     {
+        public DbSet<UserRto> Users { get; set; }
+        public DbSet<UserRoleRto> UserRoles { get; set; }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            
+        }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+        }
     }
 
-    
+
 }
