148ecfc31ee63d558bdf625461c8ee91542bf670
StudentDev <vovaklinov007@gmail.com>
2021-11-20 17:08:24 +0300

    21124
    1245235

---
+++ RubicX-Ps223020-/RubicX-Ps223020-DatAccess/DbContext/RubicContext.cs
@@ -0,0 +1,15 @@
+﻿using RubciX_Ps223020_DataAccess.Core.Interfaces.DbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubicX_Ps223020_DatAccess.DbContext
+{
+    public class RubicContext : IRubicContext
+    {
+    }
+
+    
+}
